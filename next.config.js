34da4f5b69633c9a4ec627e5e8c17cddc87a56d2
simalaX <charlesnderitu111@gmail.com>
2026-06-19 01:44:13 +0300

    fixed

--- next.config.js
+++ next.config.js
@@ -4,9 +4,10 @@ const nextConfig = {
     domains: ['res.cloudinary.com', 'images.unsplash.com'],
   },
   env: {
-    NEXT_PUBLIC_API_URL: process.env.NEXT_PUBLIC_API_URL,
+    NEXT_PUBLIC_BACKEND_URL: process.env.NEXT_PUBLIC_BACKEND_URL,
     NEXT_PUBLIC_WHATSAPP_NUMBER: process.env.NEXT_PUBLIC_WHATSAPP_NUMBER,
     NEXT_PUBLIC_CLOUDINARY_CLOUD_NAME: process.env.NEXT_PUBLIC_CLOUDINARY_CLOUD_NAME,
+    NEXT_PUBLIC_CLOUDINARY_UPLOAD_PRESET: process.env.NEXT_PUBLIC_CLOUDINARY_UPLOAD_PRESET,
   },
 }
 module.exports = nextConfig
